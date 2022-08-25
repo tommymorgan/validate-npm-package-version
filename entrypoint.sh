d5040247426ec5d273a999420d7645d8574802d3
Tommy Morgan <tommy@tommymorgan.com>
2022-08-25 14:29:21 -0500

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ version=$(jq .version package.json | sed 's/"//g')
 # See if tag exists for this version number
 version_count=$(git tag -l v$version | wc -l | xargs)
 
-if [[ "$version_count" == '1' ]]; then
+if [[ "$version_count" > 0 ]]; then
     result='failure'
 else
     result='success'
@@ -14,3 +14,9 @@ fi
 
 echo "::set-output name=version::$version"
 echo "::set-output name=result::$result"
+
+if [[ "$result" == "failure" ]]; then
+    exit 1
+else
+    exit 0
+fi
