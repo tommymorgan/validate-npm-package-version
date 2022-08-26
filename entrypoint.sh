aa5fff911404221f637be161346dd6351ee565b6
Tommy Morgan <tommy@tommymorgan.com>
2022-08-26 10:51:33 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,13 @@
 
 # Store the version number
 version=$(jq .version package.json | sed 's/"//g')
+echo "::set-output name=version::$version"
 
 # See if tag exists for this version number
 version_count=$(git tag -l $version | wc -l | xargs)
 
 if [[ "$version_count" > 0 ]]; then
-    result='failure'
-else
-    result='success'
-fi
-
-echo "::set-output name=version::$version"
-echo "::set-output name=version_count::$version_count"
-echo "::set-output name=result::$result"
-
-if [[ "$result" == "failure" ]]; then
-    exit 1
+    exit $version_count
 else
     exit 0
 fi
